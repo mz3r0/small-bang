3b7a0d2b0e1e7b1a4924b78386511fd37f4a8b07
Tugui Mihail Andrei <100568112+mz3r0@users.noreply.github.com>
2023-12-17 21:23:33 +0200

    Create clean_dup_lines.py

---
+++ Python/clean_dup_lines.py
@@ -0,0 +1,49 @@
+import argparse
+import os
+
+D_DELIMITER = '\n'
+
+parse = argparse.ArgumentParser(
+                    prog='clean_dup_lines',
+                    description='Duplicate lines in file removal. Reads whole file in memory. Assumes utf-8 encoding.',
+                    epilog='Useful for cleaning big files of links, etc.')
+
+parse.add_argument('file_path_in',
+				   help='Input path or file')
+parse.add_argument('file_path_out',
+				   help='Output path or file')
+parse.add_argument('-d','--delimiter-in',
+				   default=D_DELIMITER,
+				   help='Delimiter at INPUT, default is newline')
+parse.add_argument('-dd','--delimiter-out',
+				   default=D_DELIMITER,
+				   help='Delimiter at OUTPUT, default is newline')
+
+args = parse.parse_args()
+# print(args) # For debugging
+
+target_in = args.file_path_in
+target_out = args.file_path_out
+delimiter_in = args.delimiter_in
+delimiter_out = args.delimiter_out
+
+try:
+	if not os.path.exists(target_in):
+		raise FileNotFoundError('Input target not found!')
+
+	with open(target_in, 'r', encoding='utf-8') as fin:
+		l = fin.read().split(delimiter_in)
+		s = set(l)
+		with open(target_out, 'w', encoding='utf-8') as fout:
+			for item in s:
+				fout.write(item + delimiter_out)
+except FileNotFoundError as e:
+	print('Exiting with error message:',
+	   getattr(e,'message','N/A'))
+	exit()
+except OSError as e:
+	print('Exiting with error message:',
+	   getattr(e,'message','N/A'))
+	exit()
+
+print('Success!')
