03718f0dac922a394386e4c0fde3912881fdaf65
Tugui Mihail Andrei <100568112+mz3r0@users.noreply.github.com>
2024-03-15 20:57:33 +0200

    dataframe and series basics

--- Python Learning/2024-03-15 Pandas.py
+++ Python Learning/2024-03-15 Pandas.py
@@ -2,3 +2,18 @@
 
 import pandas as pd
 import numpy as np
+
+a_data_frame = pd.DataFrame({'color':['red','blue','green']})
+a_data_frame
+
+# series = 1D labeled array of any type
+
+s1 = pd.Series([1,2,3,4])
+s2 = pd.Series([1,2,3,np.nan])
+s3 = pd.Series(['red','green','blue'])
+s4 = pd.Series(('1',2,3))
+
+s1
+s2
+s3
+s4
