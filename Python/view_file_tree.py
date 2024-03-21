77a22112cd0a8b4368d9f9053b6ab2900aab74dc
Tugui Mihail Andrei <100568112+mz3r0@users.noreply.github.com>
2024-03-21 23:05:45 +0200

    revamp - add argparse, error handling and bugfix + refactor
    Argparse doesn't work well with exception handling due to a known issue (cpython/issues/103498). Fixed a bug where depth levels weren't calculated correctly. Added CLI support with 2 options: depth limit and spaces per indentation level.

--- Python/view_file_tree.py
+++ Python/view_file_tree.py
@@ -2,88 +2,181 @@ from pathlib import Path
 import logging
 import datetime
 import sys
+import argparse
+from os import remove
+from pprint import pprint
 
-prog_name = 'view-file-tree'
-log_filename = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S-view-file-tree.log')
+
+# HELPER FUNCTIONS
+
+def file_length(filename):
+    l = 0
+    try:
+        with open(filename) as f:
+            l = len(f.read())
+    except:
+        return -1
+    return l
+
+def close_logging(log_filename):
+    if file_length(log_filename) == 0:
+        logging.shutdown()
+        remove(log_filename)
+        return True
+    return False
+
+def print_success():
+    print('=-=-=\nSuccessful run!\n=-=-=',end='')
+
+def print_fail():
+    print('=-=-=\nExecution finished with errors! Check log!\n=-=-=',end='')
+
+
+# CONSTANTS
+
+DEFAULT_NR_OF_SPACES = 2
+DEFAULT_DEPTH = 10
+PROG_NAME = 'view-file-tree'
+LOG_FILENAME = datetime.datetime.now().strftime(f'%Y-%m-%d-%H-%M-%S-{PROG_NAME}.log')
+LOGGING_LEVEL = logging.DEBUG
+PARSER = argparse.ArgumentParser(
+    prog=PROG_NAME,
+    description='Prints the tree structure of a directory using indentation',
+    epilog='=-=-=-=-=-=-=-=-=-=-=',
+    exit_on_error=False # Doesn't work as it should https://github.com/python/cpython/issues/103498
+)
+
+# TODO Create a simpler copy of argparse
+
+PARSER.add_argument('target_dir',
+    help='Relative/absolute path to target directory')
+PARSER.add_argument('-d', '--depth', default=DEFAULT_DEPTH, type=int,
+    help=f'Print up to a given depth (inclusive), default {DEFAULT_DEPTH}')
+PARSER.add_argument('-s', '--spaces', default=DEFAULT_NR_OF_SPACES, type=int,
+    help=f'Number of spaces per indentation level, default {DEFAULT_NR_OF_SPACES}')
+
+try:
+    if len(sys.argv) == 1:
+        raise argparse.ArgumentError(None,'ArgumentError: Not enough arguments')
+    args = PARSER.parse_args()
+except argparse.ArgumentError as e:
+    print(e.message)
+    raise SystemExit(-1)
+except Exception as e:
+    print(str(e))
+    raise SystemExit(-2)
+
+
+# LOGGING CONFIGURATION
 
 logging.basicConfig(
-        level=logging.DEBUG,
+        level=LOGGING_LEVEL,
         format='%(asctime)s %(levelname)s %(message)s',
         datefmt='%Y-%m-%d %H-%M-%S',
-        filename=log_filename
+        filename=LOG_FILENAME
     )
 
-NR_OF_SPACES = 2
-indentation = ' ' * NR_OF_SPACES
+# STEP 1. VERIFY THE INPUT
 
-
-# STEP 1. Verify the input
-if len(sys.argv) == 2:
-    input_path = Path(sys.argv[1])
-else:
-    print(f'{prog_name} expects 1 path argument!')
-
-if not input_path.is_dir():
-    print('Input is not a directory!')
+if not (input_path := Path(args.target_dir)).is_dir():
+    print('Target is not a directory!')
+    close_logging(LOG_FILENAME)
     raise SystemExit(1)
 
+if (depth_limit := args.depth) < 1:
+    print('Unexpected depth value!')
+    close_logging(LOG_FILENAME)
+    raise SystemExit(2)
+
+if (nr_of_spaces := args.spaces) < 1:
+    print('Unexpected nr of spaces value!')
+    close_logging(LOG_FILENAME)
+    raise SystemExit(3)
+
+indentation = ' ' * nr_of_spaces
 print(f'Starting in {input_path}')
 
 
 # STEP 2. DFS
-path_stack = [input_path] # can only append or pop
-file_count_stack = [1]
+path_stack = list(input_path.iterdir()) # can only append or pop
+file_count_stack = [len(path_stack)]
 lvl = 0
+
+# We care about being no deeper than depth_limit - 1. Why?
+# To avoid iterating a directory whose children would all be skipped
+# and for the condition (limit_reached = lvl == depth_limit) to work
+# with the above, it is needed to decrement depth_limit here by 1
+depth_limit = depth_limit - 1
+
+logging.debug(f'depth limit: {depth_limit}')
 
 while len(path_stack) != 0 and len(file_count_stack) != 0:
     try:
+        limit_reached = lvl == depth_limit
+
+        logging.debug('')
+        logging.debug(f'lvl: {lvl}')
+        logging.debug(f'limit_reached {limit_reached}')
+        logging.debug(f'To process: {path_stack[0].name}')
+        logging.debug(f'file_count_stack: {file_count_stack}')
+        logging.debug(f'path_stack len: {len(path_stack)}')
+
         item = path_stack.pop(0)
         file_count_stack[0] -= 1
+        is_directory = item.is_dir()
 
-        print(lvl * indentation + item.name)
+
+        if not is_directory:    # print normally
+            print(lvl * indentation + item.name)
+            
+        if item.is_dir():
+            print(lvl * indentation + f'[{len(list(item.iterdir()))}] ' + item.name)
+            if not limit_reached:
+                lvl += 1
+                sub_items = list(item.iterdir())
+
+                # indentation is based on nr of files in a subdir
+                file_count_stack.insert(0,len(sub_items))
+
+                # do this instead of inserting sub_items in items
+                sub_items.extend(path_stack)
+
+                # replace old items
+                path_stack = sub_items
 
         if file_count_stack[0] == 0:
             file_count_stack.pop(0)
             lvl -= 1
 
-        if item.is_dir():
-            lvl += 1
-            sub_items = list(item.iterdir())
-
-            # indentation is based on nr of files in a subdir
-            file_count_stack.insert(0,len(sub_items))
-
-            # do this instead of inserting sub_items in items
-            sub_items.extend(path_stack)
-
-            # replace old items
-            path_stack = sub_items
-
     except PermissionError as permission_e:
         print(lvl * indentation + 'ERROR (view log)')
         logging.error(permission_e)
+        file_count_stack[0] -= 1
         continue
     except Exception as e:
         logging.critical(e)
         logging.critical(type(e))
+        file_count_stack[0] -= 1
         continue
 
+logging.debug(f'lvl after execution: {lvl}')
+logging.debug(f'file_count_stack after execution: {file_count_stack}')
 
-# STEP 3. Clean residual log if not used
-log_data_length = 0
-with open(log_filename) as f:
-    log_data_length = len(f.read())
 
-if log_data_length == 0:
-    logging.shutdown()
-    from os import remove as os_remove
-    os_remove(log_filename)
-    print('Successful run!')
+# STEP 3. Clean residual log file if not used
+
+if close_logging(LOG_FILENAME):
+    print_success()
     raise SystemExit(0)
+elif not LOGGING_LEVEL == logging.DEBUG:
+    print_fail()
+    raise SystemExit(4)
 else:
-    raise SystemExit(1)
+    print('=-=-=\nCheck log!\n=-=-=\n',end='')
+
 
 # STEP 4. Verifications
-if lvl == 0:
-    print('Successful run!')
+
+if lvl == 0 or (depth_limit == 0 and lvl == -1): # and takes precedence but parenthesis are for clarity
+    print_success()
     raise SystemExit(0)
